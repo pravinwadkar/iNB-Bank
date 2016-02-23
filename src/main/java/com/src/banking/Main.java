0b3643140ecd4b0ae39cff53f2eb058dd9f1a604
Ashutosh Sharma <ashutosh@cirrosecure.com>
2016-02-23 18:14:15 +0530

    Java files

--- src/main/java/com/src/banking/Main.java
+++ src/main/java/com/src/banking/Main.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
+@ImportResource(value="beans.xml")
 public class Main {
 
 	public static void main(String[] args) {
