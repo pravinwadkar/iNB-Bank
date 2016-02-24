0c6dd984442439fec6faab4bc7487ee1f7ee6779
pravinwadkar <pravinwadkar@gmail.com>
2016-02-24 18:40:18 +0530

    c

--- src/main/java/com/src/banking/controller/AdminController.java
+++ src/main/java/com/src/banking/controller/AdminController.java
@@ -1,12 +1,16 @@
 package com.src.banking.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.src.banking.entity.Information;
+import com.google.gson.Gson;
+import com.src.banking.dao.CustomerDao;
+import com.src.banking.entity.Customer;
+
 
 /**
  * This url mapping can be used to provide different types of information. one
@@ -18,14 +22,27 @@ import com.src.banking.entity.Information;
 @RestController
 @RequestMapping(value="/admin")
 public class AdminController {
-
-	@RequestMapping(value = "/login", method = RequestMethod.PUT,consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	
+	@Autowired
+	private CustomerDao customerdao;
+	
+	private Gson gson = new Gson();
+	/*@RequestMapping(value = "/login", method = RequestMethod.PUT,consumes=MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Information getAboutData(@RequestBody Information information) {
 //		Information information = new Information();
 		information.setName("about the bank");
 		information.setContents("This is the contents about the bank");
 		System.out.println("Working admin ***********************************" + information.getName());
 		return information;
+	}*/
+	
+	@RequestMapping(value="/login", method=RequestMethod.PUT,consumes="application/json")
+	public String createAccountDetails(@RequestBody Customer customer)
+	{
+		Customer customerData = null;
+		System.out.println(customer.getUsername());
+		customerData = customerdao.findCustomerByUsernameAndPassword(customer.getUsername(), customer.getPassword());
+		return gson.toJson(customerData.getId());
 	}
 
 }
--- src/main/java/com/src/banking/dao/CustomerDao.java
+++ src/main/java/com/src/banking/dao/CustomerDao.java
@@ -1,28 +1,39 @@
 package com.src.banking.dao;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.src.banking.entity.Customer;
 
+@Repository
 public class CustomerDao {
 	
+	@Autowired
 	private SessionFactory sessionFactory;
 	
-	public void setSessionFactory(SessionFactory sessionFactory) {
+	/*public void setSessionFactory(SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
-	}
+	}*/
 	
+	@Transactional()
 	public Customer findCustomerByUsernameAndPassword(String username,String password) {
 		Session session = null;
 		Customer cust = null;
 		session = sessionFactory.openSession();
 		Criteria cr = session.createCriteria(Customer.class);
 		cr.add(Restrictions.ilike("username", username));
-		cr.add(Restrictions.ilike("password", username));
+		cr.add(Restrictions.ilike("password", password));
+		List<Customer> results = cr.list();
+		if(results != null && results.size() > 0){
 		cust = (Customer)cr.list().get(0);
+		}
 		return cust;
 	}
 
