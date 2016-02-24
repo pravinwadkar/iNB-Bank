ca4b360ab95b681f07c0030e04d84f7ffa361200
Ashutosh Sharma <ashutosh@cirrosecure.com>
2016-02-24 15:42:37 +0530

    Added ClientController

--- src/main/java/com/src/banking/controller/ClientController.java
+++ src/main/java/com/src/banking/controller/ClientController.java
@@ -1,15 +1,16 @@
 package com.src.banking.controller;
 
-import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.src.banking.entity.Account;
-import com.src.banking.entity.Client;
+import com.src.banking.entity.Branch;
+import com.src.banking.entity.Customer;
 import com.src.banking.service.impl.ClientServiceImpl;
 
 @RestController
@@ -19,21 +20,44 @@ public class ClientController {
 	@Autowired
 	ClientServiceImpl clientService;
 
-	@RequestMapping(value = "/authorisation/login/{clientId}", method = RequestMethod.GET, 
-			consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Account login(/*@RequestBody Client client*/) {
-
-		Account account = new Account();
-		Client client = new Client();
-		client.setClientId(123);
-		client.setClientName("xoriant name ");
-		//if (client.getClientId() != 0) {
-			// validate and authorized client
-			clientService.isValidClient(client);
-			// get and return client account information
-
-		//}
-
+	/**
+	 * This URL will check in DB if the user is valid or not. If valid provides
+	 * the userID back. If login is valid the client login screen will be
+	 * displayed.
+	 * 
+	 * @param loginId
+	 * @return
+	 */
+	@RequestMapping(value = "/clientLogin/{loginId}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Account login(@PathVariable(value = "loginId") int loginId) {
+		System.out.println("loginId is  " + loginId);
+		clientService.isClientAuthorized(loginId);
 		return null;
 	}
+
+	@RequestMapping(value = "/clientHome/applyNewAccount/{enquiryId}/{email}", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Customer applyNewAccount(@PathVariable(value = "enquiryId") int enquiryId,
+			@PathVariable(value = "email") String email,
+			@RequestBody Branch branch) {
+		System.out.println("enquiryId is  " + enquiryId);
+		System.out.println("email is  " + email);
+		Customer customer = clientService.applyNewAccount(enquiryId, email,branch);
+		return customer;
+	}
+
+	@RequestMapping(value = "/viewAccountBalance/{clientId}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Account viewAccountBalance(@PathVariable(value = "clientId") int clientId) {
+		System.out.println("clientId is  " + clientId);
+		Account account = clientService.viewAccountBalance(clientId);
+		return account;
+	}
+	
+	
+	@RequestMapping(value = "/clientHome/{clientId}", method = RequestMethod.GET, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public Account viewAccountBalance(@PathVariable(value = "clientId") int clientId) {
+		System.out.println("clientId is  " + clientId);
+		Account account = clientService.viewAccountBalance(clientId);
+		return account;
+	}
+	
 }
