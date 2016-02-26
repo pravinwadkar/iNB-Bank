85fcdda974014edc92b5ca53afa35de0fe9abf90
Ashutosh Sharma <ashutosh@cirrosecure.com>
2016-02-26 09:53:02 +0530

    formatting

--- src/main/java/com/inb/banking/service/impl/ClientServiceImpl.java
+++ src/main/java/com/inb/banking/service/impl/ClientServiceImpl.java
@@ -9,54 +9,54 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.inb.banking.dao.ClientDao;
 import com.inb.banking.entity.Account;
-import com.inb.banking.entity.Branch;
 import com.inb.banking.entity.Customer;
 import com.inb.banking.service.ClientService;
 import com.inbbank.util.GenerateUUID;
 
 @Service
-public class ClientServiceImpl implements ClientService{
-	
+public class ClientServiceImpl implements ClientService {
+
 	@Autowired
 	ClientDao clientDao;
-	
-	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Customer registeredCustomerAccount(int accountId) {
 		return clientDao.registeredCustomerAccount(accountId);
 	}
-	
-	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Customer registeredCustomer(int clientId) {
 		return clientDao.getRegisteredCustomer(clientId);
 	}
-	
-	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public Customer isClientAuthorized(int clientId) {
 		return clientDao.getClientDetails(clientId);
 	}
 
-	/*public Customer applyNewAccount(int enquiryId, String email,Branch branch) {
-		return clientDao.applyNewAccount(enquiryId,email,branch);
-	}*/
-	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
-	public Account viewAccountBalance(int clientId){
+	/*
+	 * public Customer applyNewAccount(int enquiryId, String email,Branch
+	 * branch) { return clientDao.applyNewAccount(enquiryId,email,branch); }
+	 */
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public Account viewAccountBalance(int clientId) {
 		Account account = clientDao.viewAccountBalance(clientId);
 		return account;
 	}
-	
-	@Transactional(propagation=Propagation.REQUIRED,readOnly=false)
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Account transferMoney(Account sender, Account reciever) {
-		BigDecimal fundTransferBalance = sender.getBalance().subtract(new BigDecimal("50"))  ;
+		BigDecimal fundTransferBalance = sender.getBalance().subtract(new BigDecimal("50"));
 		sender.setBalance(fundTransferBalance);
 		return clientDao.viewAccountBalance(sender);
 	}
-	
-	@Transactional(propagation=Propagation.REQUIRED,readOnly=true)
-	public String unregisteredUser(String customer){
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = true)
+	public String unregisteredUser(String customer) {
 		return clientDao.unregisteredUser(customer);
 	}
-	
-	@Transactional(propagation=Propagation.REQUIRED,readOnly=false)
+
+	@Transactional(propagation = Propagation.REQUIRED, readOnly = false)
 	public Customer unregisteredUser(Customer account) {
 		account.setId(GenerateUUID.getRendomString());
 		return clientDao.unregisteredUser(account);
