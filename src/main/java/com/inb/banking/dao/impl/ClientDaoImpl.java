5d044ef7d1cdf73ecceb07a95391b6a074f7f20c
Ashutosh Sharma <ashutosh@cirrosecure.com>
2016-02-26 10:03:23 +0530

    review comment

--- src/main/java/com/inb/banking/dao/impl/ClientDaoImpl.java
+++ src/main/java/com/inb/banking/dao/impl/ClientDaoImpl.java
@@ -32,34 +32,7 @@ public class ClientDaoImpl {
 		Customer customer = (Customer) sessionFactory.getCurrentSession().get(Customer.class, id);
 		return customer;
 	}
-	/**
-	 * 
-	 * @param enquiryId
-	 * @param email
-	 * @return
-	 */
-	/*public Customer applyNewAccount(int enquiryId, String email, Branch branch) {
-		if (enquiryId != 0 && email != null) {
-			Session session = sessionFactory.openSession();
-			Customer customer = new Customer();
-			customer.setBranch(branch);
-			customer.setEnqid(new BigDecimal(enquiryId));
-			customer.setEmail("email address ");
-			// Transaction transaction = session.beginTransaction();
-			// as the branch doesn't exist till now
-			session.save(branch);
-			///////////////////////
-			// session.save(customer);
-			int customerId = customer.getId();
-			System.out.println(" customerId is : " + customerId);
-			// transaction.commit();
-			// session.close();
-			sessionFactory.getCurrentSession().save(customer);
-			return customer;
-		}
-		return null;
-	}*/
-
+	
 	public Account viewAccountBalance(int id) {
 		return (Account) sessionFactory.getCurrentSession().get(Account.class, id);
 	}
